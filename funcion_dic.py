e0d07f914444a2ee18eac9bb0a16eb8c4e59d20f
olidernas4 <nas451@hotmail.es>
2026-02-17 19:57:39 -0500

    primeroas

---
+++ funcion_dic.py
@@ -0,0 +1,16 @@
+def cliente_info():
+    cliente = {"nombre": "Andres", "vehiculos": "ABC123", "horas": 3}
+    ###Agregar
+    cliente["pago"] = 1500
+    ## actualizar
+    cliente.update({"horas": 4})
+    ##obtener valor
+    nombre = cliente.get("nombre")
+    claves = cliente.keys()
+    valores = cliente.values()
+    ##eliminar
+    cliente.pop("vehiculos")
+    print("cliente:", cliente)
+    print("nombre:", nombre)
+    print("valores", valores)
+cliente_info()
